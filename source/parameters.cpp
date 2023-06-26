4f6e3ae87c28da08ee17f3a5babb9f36c7cc1be8
yesill <yslmehmetbm@gmail.com>
2023-06-26 23:46:21 +0300

    CMake Update

--- source/parameters.cpp
+++ source/parameters.cpp
@@ -1,27 +1,2 @@
 #include "parameters.h"
 
-Parameters::Parameters(Vector3D acceleration, Vector3D velocity):
-    acceleration(acceleration), velocity(velocity)
-{
-
-}
-
-const Vector3D &Parameters::getAcceleration() const
-{
-    return acceleration;
-}
-
-void Parameters::setAcceleration(const Vector3D &newAcceleration)
-{
-    acceleration = newAcceleration;
-}
-
-const Vector3D &Parameters::getVelocity() const
-{
-    return velocity;
-}
-
-void Parameters::setVelocity(const Vector3D &newVelocity)
-{
-    velocity = newVelocity;
-}
